17580aa3741b4465033030bda483faa469396f86
Ignacio Basualdo <82559716+IgnacioBasu@users.noreply.github.com>
2024-05-21 11:49:30 -0300

    Agrego scripts para seccion 5

--- TP/Scripts/seccion_5.R
+++ TP/Scripts/seccion_5.R
@@ -36,3 +36,70 @@ axis(2, at = seq(0, 140, by = 5), las = 1)
 total <- sum(frecuencias1)
 proporciones <- frecuencias1 / total * 100
 
+
+
+datos1 <- datos_csv$Compra.agua.embotellada
+frecuencias2 <- table(datos1)
+
+indices_ordenados <- order(frecuencias2, decreasing = TRUE)
+frecuencias2 <- frecuencias2[indices_ordenados]
+labels <- names(frecuencias2)
+valores <- paste(frecuencias2)
+total <- sum(frecuencias2)
+proporciones <- frecuencias2 / total * 100
+proporciones <- round(proporciones, 2)
+porcentajes <- c(labels, proporciones)
+porcentajes <- paste(proporciones,"%", sep="")
+porcentajes <- paste(labels, "\n", porcentajes)
+par(mar = c(2, 2, 2, 2), cex = 0.7)
+grafico_red <- pie(x = frecuencias2, 
+                   labels = porcentajes, 
+                   clockwise = FALSE, 
+                   col = c("lightblue","purple"),
+                   main = "COMPRA DE AGUA EMBOTELLADA EN BARRIOS POPULARES\nNORTE DE ARGENTINA, 2022")
+
+grafico_red <- mtext("Fuente: Observatorio Villero, 2022",
+                     side = 1,
+                     line = 0,
+                     at = NA,
+                     adj = 0,
+                     cex = NA)
+
+datos3 <- datos_csv$Litros.del.tanque
+
+datos3_filtrado <- datos3[!datos3 %in% ""]
+frecuencias3 <- table(datos3_filtrado)
+frecuencias_df <- as.data.frame(frecuencias3)
+
+colnames(frecuencias_df) <- c("Value", "Frequency")
+custom_order <- c("Más de 500 lts", "200 a 500 lts","Menos de 200 lts") 
+custom_order <- custom_order[custom_order %in% frecuencias_df$Value]
+frecuencias_df$Value <- factor(frecuencias_df$Value, levels = custom_order)
+ordered_frecuencias_df <- frecuencias_df[order(frecuencias_df$Value), ]
+ordered_frecuencias_df <- rev(ordered_frecuencias_df)
+
+grafico4 <- barplot(ordered_frecuencias_df$Frequency, 
+                    names.arg = ordered_frecuencias_df$Value, 
+                    main = "CAPACIDAD DE ALMACENAMIENTO DE AGUA (EN ALTURA) EN BARRIOS POPULARES\nNORTE DE ARGENTINA, 2022",
+                    xlab = "Cantidad de viviendas", 
+                    ylab = "Capacidad de almacenamiento",
+                    xaxt = "n",
+                    col = "lightblue",
+                    horiz = T)
+
+grafico4 <-mtext("Fuente: Observatorio Villero, 2022",
+                 side = 1,
+                 line = 4,
+                 at = NA,
+                 adj = 0,
+                 cex = NA)
+
+axis(1, at = seq(0, 60, by = 1))
+
+# Modo: 200 a 500 lts
+# Proporciones (Sin contar los que  no tienen tanque de agua)
+total <- sum(frecuencias3)
+proporciones <- frecuencias3 / total * 100
+proporciones
+
+
